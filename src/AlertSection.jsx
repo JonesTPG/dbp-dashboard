116c8d5cb2661253ccfd67ef479423e19285f4aa
JonesTPG <joonas.ryynanen1@gmail.com>
2020-11-14 13:14:45 +0200

    tentative alert section.

--- src/AlertSection.jsx
+++ src/AlertSection.jsx
@@ -2,20 +2,33 @@ import React, { useState, useEffect } from "react";
 import AirQualityAlert from "./components/AirQualityAlert";
 
 const AlertSection = () => {
-  const [fetched, setFetched] = useState(0);
+  const [alert1, setAlert1] = useState(false);
+  const [alert2, setAlert2] = useState(false);
+  const [alert3, setAlert3] = useState(false);
+  const [alert4, setAlert4] = useState(false);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setFetched(fetched + 1);
-    }, 4000);
-    return () => clearInterval(interval);
+    setTimeout(() => {
+      setAlert1(true);
+    }, 1000);
+    setTimeout(() => {
+      setAlert2(true);
+    }, 3000);
+    setTimeout(() => {
+      setAlert3(true);
+    }, 6000);
+    setTimeout(() => {
+      setAlert4(true);
+    }, 9000);
   }, []);
 
   return (
     <>
       <h4>Data Analytics Alerts</h4>
-
-      {fetched && <AirQualityAlert />}
+      {alert1 ? <AirQualityAlert></AirQualityAlert> : <p>no alerts</p>}
+      {alert2 ? <AirQualityAlert></AirQualityAlert> : null}
+      {alert3 ? <AirQualityAlert></AirQualityAlert> : null}
+      {alert4 ? <AirQualityAlert></AirQualityAlert> : null}
     </>
   );
 };
